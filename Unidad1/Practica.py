392f6e703a60555690cc9a66d3cbc53bc97db55e
DevCristobalvc <101788820+DevCristobalvc@users.noreply.github.com>
2024-08-01 20:49:19 -0500

    Update Practica.py

--- Unidad1/Practica.py
+++ Unidad1/Practica.py
@@ -8,7 +8,6 @@ def addUser (id, userName, Country, age):
     newUser = {"id": id,"userName": userName,"Country": Country, "age":age}
     users.append (newUser)
 
-
 # Add video game
 def addViegame (id, name, genere, title,  downloads):
     newUser = {"id": id, "name": name, "genere": genere, "title":title, "downloads": downloads}
@@ -19,6 +18,11 @@ def addPublishers (id, name, country, tot_sales):
     newPublishers = {"id": id,"name": name,"country": country, "tot_sales": tot_sales}
     publishers.append (newPublishers)
 
+# Delete
 
 # Add data
-addUser(1, )
+addUser(1, "cristobal", "colombia", 21)
+addUser(1, "cristobal", "colombia", 21)
+
+# Imprimir
+print(users)
