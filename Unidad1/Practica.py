54d968de41bc8e9077c78e6e361e0960b1c88087
DevCristobalvc <101788820+DevCristobalvc@users.noreply.github.com>
2024-08-01 20:32:41 -0500

    Update Practica.py

--- Unidad1/Practica.py
+++ Unidad1/Practica.py
@@ -1,1 +1,24 @@
-Print("Hola mudo")
+#Listas
+users = []
+publishers = []
+videogames = []
+
+# Add user
+def addUser (id, userName, Country, age):
+    newUser = {"id": id,"userName": userName,"Country": Country, "age":age}
+    users.append (newUser)
+
+
+# Add video game
+def addViegame (id, name, genere, title,  downloads):
+    newUser = {"id": id, "name": name, "genere": genere, "title":title, "downloads": downloads}
+    users.append (newUser)
+
+# Add publisher
+def addPublishers (id, name, country, tot_sales):
+    newPublishers = {"id": id,"name": name,"country": country, "tot_sales": tot_sales}
+    publishers.append (newPublishers)
+
+
+# Add data
+addUser(1, )
